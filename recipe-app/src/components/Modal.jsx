f5370252374d33985ac425568e3ec7a3ff6dd5c1
vittor <vittor.pelicarto137@gmail.com>
2024-01-11 23:29:55 -0300

    modal style

--- recipe-app/src/components/Modal.jsx
+++ recipe-app/src/components/Modal.jsx
@@ -2,32 +2,34 @@
 
 export const Modal = (props) => {
   return (
-    <div className="modal | flow">
-      {/* Random Image */}
-      <img src="" alt="recipe image" />
-      {/* Ingredients Section */}
-      <div className="center">
-        <h3>Ingredients:</h3>
-        <ol>
-          {/* Ingredients Items */}
-          <li>{props.items}</li>
-        </ol>
-      </div>
+    <div className="modal | center">
+      <div className="modal__content | flow">
+        {/* Random Image */}
+        <img src="" alt="recipe image" />
+        {/* Ingredients Section */}
+        <div className="center">
+          <h3>Ingredients:</h3>
+          <ol>
+            {/* Ingredients Items */}
+            <li>{props.items}</li>
+          </ol>
+        </div>
 
-      {/* Instructions Section */}
-      <div className="center">
-        <h3>Instructions</h3>
-        <p>{props.text}</p>
-      </div>
+        {/* Instructions Section */}
+        <div className="center">
+          <h3>Instructions</h3>
+          <p>{props.text}</p>
+        </div>
 
-      {/* Modal Buttons */}
-      <div className="modal__btn">
-        <button className="btn" data-type="link" onClick={props.action1}>
-          Close Modal
-        </button>
-        <button className="btn" onClick={props.action2}>
-          {props.btnContent}
-        </button>
+        {/* Modal Buttons */}
+        <div className="modal__btn">
+          <button className="btn" data-type="link" onClick={props.action1}>
+            Close Modal
+          </button>
+          <button className="btn" onClick={props.action2}>
+            {props.btnContent}
+          </button>
+        </div>
       </div>
     </div>
   )
